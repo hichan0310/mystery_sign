7437a8bab3259b693e4d68e5a8cf7fb0bfbce36b
hichan0310 <103562171+hichan0310@users.noreply.github.com>
2022-10-07 17:22:43 +0900

    23

--- Qmanager.py
+++ Qmanager.py
@@ -1,6 +1,9 @@
 import random as r
 
-#여기에 문제 추가
+
+# 여기에 문제 추가
+# 현재 23개
+
 
 class question_manager:
     def __init__(self):
@@ -16,6 +19,18 @@ class question_manager:
             [lambda a, b: (a - 1) * (b - 1), "1씩 빼서 곱하기".encode("utf-8")],
             [lambda a, b: (a + b) % 12, "더해서 12로 나눈 나머지".encode("utf-8")],
             [lambda a, b: a // b + b // a, "나누었을 때 몫".encode("utf-8")],
+            [lambda a, b: self.hardthing(a, b), "각 숫자가 합쳐서 몇 개가 있는지".encode("utf-8")],
+            [lambda a, b: (a + b) % 12, "제작자는 시계라고 하는데 솔직히 이건 좀 억지임".encode("utf-8")],
+            [lambda a, b: a // b + b // a, "몫".encode("utf-8")],
+            [lambda a, b: abs(a * a - b * b), "제곱의 차(더하고 빼고 해서 곱한 거)".encode("utf-8")],
+            [lambda a, b: (a + 1) * (b - 1), "(a+1)(b-1)".encode("utf-8")],
+            [lambda a, b: (a - 1) * (b + 1), "(a-1)(b+1)".encode("utf-8")],
+            [lambda a, b: a + a * b, "a+ab".encode("utf-8")],
+            [lambda a, b: (a + b) * a, "a(a+b)".encode("utf-8")],
+            [lambda a, b: a * b + a // b + b // a, "a*b+몫".encode("utf-8")],
+            [lambda a, b: sum(list(map(int, list(str(a + b))))), "더해서 자리수 합".encode("utf-8")],
+            [lambda a, b: self.sumeverynum(a ** b), "a^b의 자리수 합(많이 변태같음)"],
+            [lambda a, b: self.jinbub_2(a) + self.jinbub_2(b), "이진법으로 바꾸고 2개 그대로 합하기"]
         ]
         self.length = len(self.questions)
         self.index = r.randint(0, self.length)
@@ -23,3 +38,33 @@ class question_manager:
 
     def answer(self, a, b):
         return self.questions[self.index][0](a, b)
+
+    def hardthing(self, a, b):
+        ans = ''
+        alist = list(map(int, list(str(a))))
+        blist = list(map(int, list(str(b))))
+        nlist = alist + blist
+        CountList = [0] * 10
+        for i in nlist:
+            CountList[i] += 1
+        for i in range(1, 10):
+            if CountList[i]:
+                ans = str(ans) + str(i) + str(CountList[i])
+        if CountList[0]:
+            ans = str(ans) + str(0) + str(CountList[0])
+        return int(ans)
+
+    def sumeverynum(self, a):
+        return sum(list(map(int, list(str(a)))))
+
+    def jinbub_2(self, a):
+        ans = ''
+        while (a != 0):
+            if (a % 2 == 1):
+                ans = '1' + ans
+            else:
+                ans = '0' + ans
+            a = a // 2
+        if a == 0:
+            return 0
+        return int(ans)
